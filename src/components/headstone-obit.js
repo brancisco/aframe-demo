6cc8f0a02f0615918fe93bbc5a82cd6a7dffdb66
Alex Hooten <alexhooten@funeralinnovations.com>
2020-01-14 12:29:42 -0700

    moved all changes from my forked repository to the main repository to make integration easier

---
+++ src/components/headstone-obit.js
@@ -0,0 +1,17 @@
+AFRAME.registerComponent('headstone-obit', {
+    schema: {
+        tributes: { type: 'number', default: 0 }
+    },
+    init: function () {
+        // api
+        fetch('https://staging.funeralinnovations.com/obituaries/getObituary?obit_id=344935&format=json&cors=1')
+        .then(response => response.json())
+        .then(json => {
+            console.log(json)
+            // create element based on json response
+            let obittext = document.createElement('a-entity')
+            obittext.setAttribute('text', "font: kelsonsans; value: " + json.obituary.about + "; color: #000; align: center; wrapCount: 14; baseline: top")
+            this.el.appendChild(obittext)
+        })
+    }
+})
--- src/index.js
+++ src/index.js
@@ -9,3 +9,6 @@ require('axios')
 /* REQUIRE LOCAL COMPONENTS HERE */
 require('./components/hello-world')
 require('./components/multi-photo-example')
+require('./components/headstone-name')
+require('./components/headstone-dates')
+require('./components/headstone-obit')
