e1c9088584000cb5f2b0b4a36e38067d3428dd67
Brandon Aguirre <brag4918@colorado.edu>
2020-01-14 13:18:31 -0700

    nit: remove console logs

--- src/components/sub-component-example.js
+++ src/components/sub-component-example.js
@@ -5,13 +5,8 @@ AFRAME.registerComponent('sub-component-example', {
         rotation: { type: 'vec3' }
     },
     init: function () {
-        console.log({data: this.data})
         this.image = document.createElement('a-image')
         this.image.setAttribute('src', `url(${this.data.url})`)
-        console.log({
-            innerpos: Object.values(this.data.position).join(' '),
-            innerrot: Object.values(this.data.rotation).join(' ')
-        })
         this.image.setAttribute('position', Object.values(this.data.position).join(' '))
         this.image.setAttribute('rotation', Object.values(this.data.rotation).join(' '))
         this.el.appendChild(this.image)
