6122ba3292954dc4102a1bb03dfc85f80ec0516d
Brandon Aguirre <brag4918@colorado.edu>
2020-01-14 12:32:13 -0700

    stub out tribute garden +example

--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -1,5 +1,15 @@
 AFRAME.registerComponent('hello-world', {
     init: function () {
         console.log('Hello, World!')
+        console.log(UTILS.formatProps([
+            {
+                name: 'my-prop-name',
+                value: '4 3 2' 
+            },
+            {
+                name: 'cool-prop',
+                value: 'hello this is a prop' 
+            }
+        ]))
     }
 })
