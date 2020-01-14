73fba883019576f53088a21bf6ff315cb1357b73
Brandon Aguirre <brag4918@colorado.edu>
2020-01-14 09:29:12 -0700

    add guids for how to add components to index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+/* AFRAME LIBRARY SHOULD BE THE FIRST THING IMPORTED */
 require('aframe')
+
+/* REQUIRE ANY node_module LEVEL COMPONENTS HERE */
+// require('example-component')
+
+/* REQUIRE LOCAL COMPONENTS HERE */
+require('Components/hello-world')
