0c171be0a3cbdfded8faa0eecfab3a2503cc29cb
jamesarama <james@funeralinnovations.com>
2020-01-15 00:15:00 -0800

    - simulate tree growth

--- src/components/tributes-garden.js
+++ src/components/tributes-garden.js
@@ -43,7 +43,30 @@ AFRAME.registerComponent('tributes-garden', {
                             position = trib.tribute.data.meta.vr_tree_position;
                             model = document.createElement('a-obj-model');
                             model.setAttribute('src', '#tree-obj');
-                            model.setAttribute('mtl', '#tree-mtl')
+                            model.setAttribute('mtl', '#tree-mtl');
+
+                            // scale tree based on its age
+                            let scale = {x: 0.1, y: 0.1, z: 0.1};
+
+                            // Tree growth simulation. We're using a 10 hr window for quick progression of growth
+                            let maturityTime = 10; // in hrs
+                            let birthDate = new Date(trib.tribute.datetime);
+                            let today = new Date();
+                            let elapsedTime = today - birthDate;
+                            let elapsedHours = Math.floor(Math.abs(elapsedTime / 1000 / 60 / 60));
+
+                            // scale up each hour
+                            if (elapsedHours >= 0 && elapsedHours < maturityTime) {
+                                scale.x += elapsedHours * 0.1;
+                                scale.y += elapsedHours * 0.1;
+                                scale.z += elapsedHours * 0.1
+                            } else {
+                                scale.x = 1;
+                                scale.y = 1;
+                                scale.z = 1
+                            }
+
+                            model.setAttribute('scale', scale)
                         }
 
                         // set position
