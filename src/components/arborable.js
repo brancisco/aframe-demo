ce2890597f1c357ddb42e66fae9614baa911e95a
Brandon Aguirre <brag4918@colorado.edu>
2020-01-14 17:52:19 -0700

    add: pass scaleFrom and scaleTo as parameters

--- src/components/arborable.js
+++ src/components/arborable.js
@@ -2,14 +2,13 @@ import axios from 'axios'
 
 AFRAME.registerComponent('arborable', {
     schema: {
-
+        scaleFrom: { type: 'vec3', default: { x: 0.02, y: 0.02, z: 0.02 } },
+        scaleTo: { type: 'vec3', default: { x: 0.1, y: 0.1, z: 0.1 } }
     },
     init: function () {
         let scene = this.el.sceneEl
         let el = this.el
-        let defaults = {
-            scale: '0.02 0.02 0.02'
-        }
+        
         // TODO: these should be loaded from the obituary - then make sure we're not placing where
         // other people have placed tributes before..
         this.positions = []
@@ -51,12 +50,14 @@ AFRAME.registerComponent('arborable', {
             // add tree to scene
             this.positions.push(evt.detail.intersection.point)
             let tree = document.createElement('a-obj-model')
-            tree.setAttribute('scale', defaults.scale)
+            tree.setAttribute('scale', Object.values(this.data.scaleFrom).join(' '))
             tree.setAttribute('position', Object.values(evt.detail.intersection.point).join(' '))
             tree.setAttribute('rotation', '0 ' + Math.floor(Math.random() * 360) + ' 0')
+
+            // todo: the src and mtl should be passed as a parameter in the schema
             tree.setAttribute('src', '#tree-obj')
             tree.setAttribute('mtl', '#tree-mtl')
-            tree.setAttribute('animation', "property: scale; to: 0.1 0.1 0.1; loop: false; dur: 3000")
+            tree.setAttribute('animation', `property: scale; to: ${Object.values(this.data.scaleTo).join(' ')}; loop: false; dur: 3000`)
             scene.appendChild(tree)
         });
     }
